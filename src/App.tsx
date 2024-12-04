eebc2c4c9308db96a753456e91c43bf0df2d4831
Fredrik Johansen <fredrik1206@gmail.com>
2024-12-04 19:19:59 +0100

    feat(app): adds router to App.tsx file

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,48 @@
-import { Editor } from "./components/Editor";
+import {
+  createRootRoute,
+  createRoute,
+  createRouter,
+  Outlet,
+  RouterProvider,
+} from "@tanstack/react-router";
+import { TanStackRouterDevtools } from "@tanstack/router-devtools";
+import { HomePage } from "./pages/Home";
+import { EditorPage } from "./pages/Editor";
 
-function App() {
-  return (
+const rootRoute = createRootRoute({
+  component: () => (
     <main className="min-h-screen w-full">
-      <Editor />
+      <Outlet />
+      <TanStackRouterDevtools />
     </main>
-  );
+  ),
+});
+
+const homeRoute = createRoute({
+  getParentRoute: () => rootRoute,
+  path: "/",
+  component: () => <HomePage />,
+});
+
+const editorRoute = createRoute({
+  getParentRoute: () => rootRoute,
+  path: "/editor",
+  component: () => <EditorPage />,
+});
+
+const routeTree = rootRoute.addChildren([homeRoute, editorRoute]);
+
+const router = createRouter({ routeTree });
+declare module "@tanstack/react-router" {
+  interface Register {
+    router: typeof router;
+  }
 }
 
-export default App;
+export const App = () => {
+  return (
+    <>
+      <RouterProvider router={router} />
+    </>
+  );
+};
