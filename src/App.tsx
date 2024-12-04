e98036089e9aa2aa9c34d50ddd351f36131603bb
Fredrik Johansen <fredrik1206@gmail.com>
2024-12-04 19:04:48 +0100

    feat(editor): adds simple plate editor

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,9 @@
-import { useState } from "react";
-import { invoke } from "@tauri-apps/api/core";
-import { Button } from "./shadcn-ui/components/ui/button";
+import { Editor } from "./components/Editor";
 
 function App() {
-  const [greetMsg, setGreetMsg] = useState("");
-  const [name, setName] = useState("");
-
-  async function greet() {
-    setGreetMsg(await invoke("greet", { name }));
-  }
-
   return (
-    <main>
-      <h1>Welcome to Tauri + React</h1>
-      <p>Click on the Tauri, Vite, and React logos to learn more.</p>
-
-      <form
-        className="flex flex-col w-52"
-        onSubmit={(e) => {
-          e.preventDefault();
-          greet();
-        }}
-      >
-        <input
-          className="border border-black/50"
-          onChange={(e) => setName(e.currentTarget.value)}
-          placeholder="Enter a name..."
-        />
-        <Button type="submit">Greet</Button>
-      </form>
-      <p className="text-base font-bold">{greetMsg}</p>
+    <main className="min-h-screen w-full">
+      <Editor />
     </main>
   );
 }
