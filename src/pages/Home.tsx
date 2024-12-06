d1a8997fd1887e3feb185921b001c86057e4f528
Fredrik Johansen <fredrik1206@gmail.com>
2024-12-06 23:03:50 +0100

    feat(pages): adds functionality to open file from dialog

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,10 +1,35 @@
-import { Link } from "@tanstack/react-router";
+import { Button } from "@shadcn-ui/components/ui/button";
+import { Link, useNavigate } from "@tanstack/react-router";
+import { open } from "@tauri-apps/plugin-dialog";
+import { useEditor } from "../providers/Editor";
 
 export const HomePage = () => {
+  const navigate = useNavigate();
+  const editor = useEditor();
+
+  const openFile = async () => {
+    const filePath = await open({
+      multiple: false,
+      directory: false,
+      title: "Open Note",
+      filters: [
+        {
+          name: "note-file-extension",
+          extensions: ["note"],
+        },
+      ],
+    });
+    editor.setFilePath(filePath);
+    navigate({
+      to: "/editor",
+    });
+  };
+
   return (
     <div className="flex flex-col justify-center items-center">
       <span>Home Page</span>
       <Link to="/editor">Editor</Link>
+      <Button onClick={async () => await openFile()}>Open File</Button>
     </div>
   );
 };
