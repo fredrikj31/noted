990468ad187ad32c91a3a6e9a6e26381908edeac
Fredrik Johansen <fredrik1206@gmail.com>
2024-12-06 23:04:12 +0100

    feat(pages): logs out opened file on editor page

--- src/pages/Editor.tsx
+++ src/pages/Editor.tsx
@@ -1,7 +1,15 @@
 import { Link } from "@tanstack/react-router";
 import { Editor } from "../components/Editor";
+import { useEditor } from "../providers/Editor";
+import { useEffect } from "react";
 
 export const EditorPage = () => {
+  const editor = useEditor();
+
+  useEffect(() => {
+    console.log({ filePath: editor.filePath });
+  }, [editor.filePath]);
+
   return (
     <div className="flex flex-col size-full">
       <Editor />
