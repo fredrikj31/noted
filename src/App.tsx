1d49a1bd0bf9120eb7a27c28e71f80850d958dea
Fredrik Johansen <fredrik1206@gmail.com>
2024-12-23 14:23:49 +0100

    feat(app): adds shadcn button to page

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
+import { Button } from "@shadcn/components/ui/button";
+
 export const App = () => {
   return (
     <>
       <h1 className="text-red-500 text-xl">Hello World</h1>
+      <Button onClick={() => console.log("Hello World")}>Click Me</Button>
     </>
   );
 };
